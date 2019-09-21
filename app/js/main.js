55cb2d35d259400bff662c304bceb370ab145207
Phong Truong <pr2030@me.com>
2019-09-22 03:23:20 +0700

    photos completed

---
+++ app/js/main.js
@@ -0,0 +1,15 @@
+// document.addEventListener("DOMContentLoaded", function() {
+//   var elems = document.querySelectorAll(".carousel");
+//   var instances = M.Carousel.init(elems, options);
+// });
+
+// Or with jQuery
+
+// $(document).ready(function() {
+//   $(".carousel").carousel();
+// });
+
+jQuery(document).ready(function($) {
+  //you can now use $ as your jQuery object.
+  $(".carousel").carousel();
+});
---
+++ app/js/sample01.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $(".carousel").carousel({
+    // carouselWidth: 1500,
+    // carouselHeight: 700,
+    carouselWidth: 500,
+    carouselHeight: 700,
+    directionNav: true,
+    shadow: true,
+    buttonNav: "bullets"
+  });
+});
